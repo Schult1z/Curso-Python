628e7999421df511e0a371beedfdfb49868ea4c2
Gabriel Schultz do Amaral <gab12schultz@hotmail.com>
2026-09-18 14:55:01 -0300

    Create inicio.py

---
+++ Python_Basico/Conceitos-Iniciais/inicio.py
@@ -0,0 +1,28 @@
+#Variaveis
+print("Ola, Mundo!")
+print(10)
+
+#Tipos de Dados
+
+#string = texto
+print(type("Ola, Mundo!"))
+#int = numero
+print(type(10))
+# float = num decimais
+print(type(3.14))
+
+name = "Gabriel"
+Name = "Schultz"
+idade = 20
+print(name)
+print(idade)    
+print(Name)
+
+#Input (entadas)
+lastname = input("Digite seu sobrenome: ")
+print(lastname)
+
+number1 = int(input("Digite um numero: "))
+number2 = int(input("Digite outro numero: "))
+resultado = number1 + number2
+print(f'A soma entre {number1} e {number2} = {resultado}')
