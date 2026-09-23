81a389cc6eeed6658a82eed49e4889116e788e41
Gabriel Schultz Do Amaral <gabriel.amaral@telefonica.com>
2026-09-23 14:38:30 -0300

    Update Macth

---
+++ Python_Basico/Conceitos-Iniciais/macth.py
@@ -0,0 +1,11 @@
+opcao = int(input("Escolha uma opção: 1 a 3 " ))
+match opcao: 
+    case 1: 
+        print("você escolheu a opção 1 ")
+    case 2:
+        print("você escolheu a opção 2 ")
+    case 3: 
+        print("você escolheu a opção 3 ")
+    case _:
+        print("Opção inválida !")
+        
