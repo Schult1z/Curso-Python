f6dce2efdf015a2c6bf3bcd3ecb3c704d7e81972
Gabriel Schultz Do Amaral <gabriel.amaral@telefonica.com>
2026-09-23 14:31:04 -0300

    Update

--- Python_Basico/Conceitos-Iniciais/condicionais.py
+++ Python_Basico/Conceitos-Iniciais/condicionais.py
@@ -3,3 +3,11 @@ idade = int(input("Qual é a sua idade ? "))
 if idade < 18:
     print ("Você é menor de idade !")
 else: print("Você é maior de idade !!")
+
+###
+
+numero = float(input("Digite um número: " ))
+if numero % 2 == 0:
+    print("Esse número é par")
+else: 
+    print ("Esse número é impar!")
